f7fbb9f1e6ee7a116c68419f6b956ef8dab33dc9
白应非 <diefrom.me@gmail.com>
2023-04-14 11:12:21 +0800

    Update user.groovy

--- init-groovys/user.groovy
+++ init-groovys/user.groovy
@@ -5,7 +5,7 @@ import hudson.security.*
 def instance = Jenkins.getInstance()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "lei@jenkins+10086@yu.com")
+hudsonRealm.createAccount("admin", "admin")
 instance.setSecurityRealm(hudsonRealm)
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
